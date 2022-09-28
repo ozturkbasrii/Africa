e2fe744285ec28b6ba938a3b8d61f9fa055cc48c
ozturkbasrii <ozturkbasrii@gmail.com>
2022-09-28 21:15:54 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Hasan Basri Öztürk on 28.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
